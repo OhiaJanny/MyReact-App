f7a4059b8a1518054346114fefa718354711ed66
OhiaJanny <janechidera12@gmail.com>
2022-07-26 15:41:03 +0100

    First React App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+// import "./App.css";
+// import Counter from "./components/Counter";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <h1>Hello PWC Trainees</h1>
+//       <Counter />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+// import "./App.css";
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import StudentsData from "./pages/StudentsData";
+// import Header from "./components/Header";
+// import Footer from "./components/Footer";
+// import Home from "./pages/Home";
+// import Gallery from "./pages/Gallery";
+// import AboutUs from "./pages/Aboutus";
+// import Applicationform from "./pages/Applicationform";
+// import Contactus from "./pages/Contactus";
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <BrowserRouter>
+//         <Header />
+//         <Routes>
+//           <Route path={"/Home"} element={<Home />} />
+//           <Route path={"/Aboutus"} element={<AboutUs />} />
+//           <Route path={"/Gallery"} element={<Gallery />} />
+//           <Route path={"/studentsData"} element={<StudentsData />} />
+//           <Route path={"/Contactus"} element={<Contactus />} />
+//           <Route path={"/Applicationform"} element={<Applicationform />} />
+//         </Routes>
+//         <Footer />
+//       </BrowserRouter>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import "./App.css";
+import Recruitmentform from "./pages/Recruitmentform";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello PWC Trainees</h1>
+      <Recruitmentform />
     </div>
   );
 }
